f4866b3ef68dcfbc73d604ca0ec8a2241a5e553b
GabrielTrigo <gabrieltrigo123@hotmail.com>
2025-11-01 13:40:47 -0300

    ...

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,31 +1,3 @@
-terraform {
-  # required_version = ">= 1.13.4"
-
-  required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "~> 2.68.0"
-    }
-  }
-
-  backend "s3" {
-    endpoints = {
-      s3 = "https://sfo3.digitaloceanspaces.com"
-    }
-
-    bucket = "trigus"
-    key    = "terraform.tfstate"
-
-    # Deactivate a few AWS-specific checks
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    skip_s3_checksum            = true
-    region                      = "us-east-1"
-  }
-}
-
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
   token             = var.do_token
@@ -37,14 +9,26 @@ module "droplet" {
   source     = "./modules/droplet"
   env_name   = var.env_name
   web_key_id = module.ssh.web_key_id
+
+  providers = {
+    digitalocean = digitalocean
+  }
 }
 
 module "network" {
   source     = "./modules/network"
   env_name   = var.env_name
   droplet_id = module.droplet.droplet_id
+
+  providers = {
+    digitalocean = digitalocean
+  }
 }
 
 module "ssh" {
   source = "./modules/ssh"
+
+  providers = {
+    digitalocean = digitalocean
+  }
 }
