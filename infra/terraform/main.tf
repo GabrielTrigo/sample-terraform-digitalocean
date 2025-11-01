2228d9dee811900fb3953c668fe65d0eb65a9781
GabrielTrigo <gabrieltrigo123@hotmail.com>
2025-11-01 13:20:06 -0300

    ...

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,3 +1,31 @@
+terraform {
+  required_version = ">= 1.13.4"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.68.0"
+    }
+  }
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://sfo3.digitaloceanspaces.com"
+    }
+
+    bucket = "trigus"
+    key    = "terraform.tfstate"
+
+    # Deactivate a few AWS-specific checks
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_s3_checksum            = true
+    region                      = "us-east-1"
+  }
+}
+
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
   token             = var.do_token
